437c983d9abbd2e1a55293775f330750a6220015
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-15 03:29:19 +0530

    change in index

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
-var CLIENTS=[];
+// var CLIENTS=[];
 wss.on('connection', function connection(ws) {
-    CLIENTS.push(ws);
+    // CLIENTS.push(ws);
     ws.on('message', function message(data) {
       // console.log('received: %s', data);
-      sendAll(data);
-    //   wss.clients.forEach(function each(client) {
-    //       client.send(data);
-    //    });
+      // sendAll(data);
+      wss.clients.forEach(function each(client) {
+          client.send(data);
+       });
   
     });
   
@@ -49,6 +49,6 @@ function sendAll (message) {
 //   });
 // });
 
-server.listen(3000, function () {
+server.listen(8080, function () {
   console.log('Listening on http://0.0.0.0:8080');
 });
