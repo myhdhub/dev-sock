3e08dcb06d6b188feb3a680f3bba88576b8711a2
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-15 03:21:14 +0530

    change in index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var CLIENTS=[];
 wss.on('connection', function connection(ws) {
     CLIENTS.push(ws);
     ws.on('message', function message(data) {
-      console.log('received: %s', data);
+      // console.log('received: %s', data);
       sendAll(data);
     //   wss.clients.forEach(function each(client) {
     //       client.send(data);
@@ -49,6 +49,6 @@ function sendAll (message) {
 //   });
 // });
 
-server.listen(8080, function () {
+server.listen(3000, function () {
   console.log('Listening on http://0.0.0.0:8080');
 });
