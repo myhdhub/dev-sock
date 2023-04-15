caa98316f66f846e34cf327cc0b8f73d80a3517e
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-15 13:09:37 +0530

    change in index

--- index.js
+++ index.js
@@ -11,15 +11,34 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const wssSockApi = new WebSocket('wss://jellyfish-app-uati5.ondigitalocean.app');
+wssSockApi.binaryType = "arraybuffer"; 
+
 var CLIENTS=[];
 wss.on('connection', function connection(ws) {
     CLIENTS.push(ws);
-    ws.on('message', function message(data) {
-      console.log('received: %s', data);
-      sendAll(data);
+    ws.on('message', function message(messageData) {
+      console.log('received: %s', messageData);
+      sendAll(messageData);
     //   wss.clients.forEach(function each(client) {
     //       client.send(data);
     //    });
+
+    var enc = new TextDecoder("utf-8");
+                            
+      const decodeData = enc.decode(messageData.data); 
+      let recievedData = JSON.parse(decodeData);
+      if(recievedData.pageData) {
+        console.log("uint8===",JSON.parse(decodeData));
+        // connect("ws://148.251.21.118:5570");
+        wssSockApi.send(messageData.data);
+        // client.close();
+
+        // connect("ws://148.251.21.118:5570");
+        // location.href = '/'
+    
+      }
   
     });
   
