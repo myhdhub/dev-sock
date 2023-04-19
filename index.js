a9d1e87ced0285051d4efb138be0ec1d0d0470f2
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-19 12:32:54 +0530

    change in index

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ const connect = (endpoint,isReload) => {
       // setTimeout(() => {
       //   connect("ws://148.251.21.118:5570");
       // }, 1000);
-      // connect("ws://148.251.21.118:5570",false);
+      connect("ws://148.251.21.118:5570",false);
     };
 
     client.onerror = (error) => {
