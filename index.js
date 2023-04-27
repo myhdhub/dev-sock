c8367a4c2c58f47d833717430dd6cc72d9a81413
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-27 15:12:17 +0530

    change in index

--- index.js
+++ index.js
@@ -86,123 +86,114 @@ function sendAll (message) {
 //   });
 // });
 
-pm2.connect(function(err) {
-  if (err) throw err;
-
-setTimeout(function worker() {
-  console.log("Restarting app...");
-  pm2.restart('app', function() {});
-  setTimeout(worker, 300000);
-  }, 300000);
-});
 
 server.listen(8080, function () {
   console.log('Listening on http://0.0.0.0:8080');
 });
 
-const connect = (endpoint,isReload) => {
+// const connect = (endpoint,isReload) => {
   
   
 
-  try {
+//   try {
 
-    let options = {
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36"
-      }
-    };
+//     let options = {
+//       headers: {
+//         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36"
+//       }
+//     };
 
-    let client = new WebSocket(endpoint, options);
-    client.binaryType = "arraybuffer";
+//     let client = new WebSocket(endpoint, options);
+//     client.binaryType = "arraybuffer";
 
     
 
-    client.onopen = () => {
-      console.log("websocket open");
+//     client.onopen = () => {
+//       console.log("websocket open");
 
       
-        client.send(JSON.stringify({"requestType":1,"sessionKey":"","Data":{"Firm":"ALLOW","PrivateKey":"TT@90","ApiKey":"CRIC@20"}}));
-      //   client.send("42" + JSON.stringify(["getRooms", false]));
+//         client.send(JSON.stringify({"requestType":1,"sessionKey":"","Data":{"Firm":"ALLOW","PrivateKey":"TT@90","ApiKey":"CRIC@20"}}));
+//       //   client.send("42" + JSON.stringify(["getRooms", false]));
 
-      // wssMySock.onmessage = (event) => {
-      //   var enc = new TextDecoder("utf-8");
+//       // wssMySock.onmessage = (event) => {
+//       //   var enc = new TextDecoder("utf-8");
                               
-      //   const decodeData = enc.decode(event.data); 
-      //   let recievedData = JSON.parse(decodeData);
-      //   if(recievedData.pageData) {
-      //     client.close();
+//       //   const decodeData = enc.decode(event.data); 
+//       //   let recievedData = JSON.parse(decodeData);
+//       //   if(recievedData.pageData) {
+//       //     client.close();
 
-      //     client.send(JSON.stringify({"requestType":1,"sessionKey":"","Data":{"Firm":"ALLOW","PrivateKey":"TT@90","ApiKey":"CRIC@20"}}));
-      //   }
-      // };
+//       //     client.send(JSON.stringify({"requestType":1,"sessionKey":"","Data":{"Firm":"ALLOW","PrivateKey":"TT@90","ApiKey":"CRIC@20"}}));
+//       //   }
+//       // };
 
-    };
+//     };
 
-    client.onmessage = (event) => {
-      console.log("main-msg---",event.data);
+//     client.onmessage = (event) => {
+//       console.log("main-msg---",event.data);
 
-        // setInterval(() => {
-        //   feedData = [];
-        //   client.close();
-        // //   // console.log("length--", feedData.length);
-        // //   if(feedData.length > 1000) {
-        // //     let feedDataLength = feedData.length / 2;
-        // //     feedData.splice(0, feedDataLength);
-        // //   }
-        // // }, 900000);
-        // }, 900000);
+//         // setInterval(() => {
+//         //   feedData = [];
+//         //   client.close();
+//         // //   // console.log("length--", feedData.length);
+//         // //   if(feedData.length > 1000) {
+//         // //     let feedDataLength = feedData.length / 2;
+//         // //     feedData.splice(0, feedDataLength);
+//         // //   }
+//         // // }, 900000);
+//         // }, 900000);
 
-        feedData.push(event.data);
+//         feedData.push(event.data);
 
-        // if(feedData.length > 500) {
-        //   feedData.shift();
-        // }
+//         // if(feedData.length > 500) {
+//         //   feedData.shift();
+//         // }
 
-        sendAll(event.data);  
-        // wss.send(event.data);
+//         sendAll(event.data);  
+//         // wss.send(event.data);
         
-      // }, 1000);
+//       // }, 1000);
      
-    };
+//     };
 
-    client.onclose = (reason) => {
-      console.log("websocket close:", reason.code);
-      // setTimeout(() => {
-      //   connect("ws://148.251.21.118:5570");
-      // }, 1000);
-      // connect("ws://148.251.21.118:5570",false);
-    };
+//     client.onclose = (reason) => {
+//       console.log("websocket close:", reason.code);
+//       // setTimeout(() => {
+//       //   connect("ws://148.251.21.118:5570");
+//       // }, 1000);
+//       // connect("ws://148.251.21.118:5570",false);
+//     };
 
-    client.onerror = (error) => {
-      console.log("websocket error:", error.message);
-    };
+//     client.onerror = (error) => {
+//       console.log("websocket error:", error.message);
+//     };
 
-    wssMySock.onmessage = (event) => {
-      console.log("aa",event.data);
-      // var enc = new TextDecoder("utf-8");
+//     wssMySock.onmessage = (event) => {
+//       console.log("aa",event.data);
+//       // var enc = new TextDecoder("utf-8");
                             
-      // const decodeData = enc.decode(event.data); 
-      // let recievedData = JSON.parse(decodeData);
-      if(event.data.pageData) {
-        console.log("force close");
+//       // const decodeData = enc.decode(event.data); 
+//       // let recievedData = JSON.parse(decodeData);
+//       if(event.data.pageData) {
+//         console.log("force close");
         
-        client.close();
+//         client.close();
 
-        console.log("uint8===",JSON.parse(decodeData));
-        // connect("ws://148.251.21.118:5570");
-        // client.close();
+//         console.log("uint8===",JSON.parse(decodeData));
+//         // connect("ws://148.251.21.118:5570");
+//         // client.close();
 
-        // connect("ws://148.251.21.118:5570");
-        // location.href = '/'
+//         // connect("ws://148.251.21.118:5570");
+//         // location.href = '/'
     
-      }
-    // wss.send(event.data);
-    //   getMessage(event.data);
-    };
+//       }
+//     // wss.send(event.data);
+//     //   getMessage(event.data);
+//     };
     
-  } catch (error) {
-    console.log("connect error:", error.message);
-  }
-};
+//   } catch (error) {
+//     console.log("connect error:", error.message);
+//   }
+// };
 
-connect("ws://148.251.21.118:5570", false);
+// connect("ws://148.251.21.118:5570", false);
