ffd6a6abd890a2d952628ef361b8d1c755ab1deb
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-27 14:18:45 +0530

    change in index

--- index.js
+++ index.js
@@ -15,12 +15,10 @@ const server = createServer(app);
 const wss = new WebSocket.Server({ server });
   
   const wssMySock = new WebSocket('wss://hammerhead-app-hq3tv.ondigitalocean.app');
+  const sockApi = new WebSocket('ws://148.251.21.118:5570');
   // const wssMySock = new WebSocket('wss://localhost:3000');
     wssMySock.binaryType = "arraybuffer"; 
 
-    var feedData = [];
-  
-  var CLIENTS=[];
 
     wss.getUniqueID = function () {
       function s4() {
@@ -90,12 +88,81 @@ server.listen(8080, function () {
 runServer();
 
 
+setInterval(() => {
 
+  // closeSockApi();
+  
+  runServerSetInterval();
+    
+  }, 60000);
 
 function runServer() {
 
-  const sockApi = new WebSocket('ws://148.251.21.118:5570');
-
+  var feedData = [];
+  
+  var CLIENTS=[];
+  
+  wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+  };
+  
+  wss.on('connection', function connection(ws) {
+  
+      ws.id = wss.getUniqueID();
+  
+      CLIENTS.push(ws);
+  
+      ws.on('message', function message(messageData) {
+        let msg = JSON.parse(messageData);
+        console.log('received: %s', msg);
+        // console.log('received msg from: %s', ws);
+  
+        wss.clients.forEach(function each(client) {
+          if(client.id == ws.id) {
+              if(feedData.length > 0){
+              feedData.forEach(message => {
+                client.send(message);
+              });
+            }
+          }
+         });
+  
+        // if(messageData.pageData) {
+        // console.log('pageeee');
+  
+        // }
+  
+        // ws.close();
+        // ws.pong
+        // var msg = JSON.parse(messageData);
+        // sendAll(messageData);
+      //   wss.clients.forEach(function each(client) {
+        // wssMySock.send(messageData);
+      //    });
+        // if(messageData.pageData) {
+          // wssMySock.send(messageData);
+        // }
+        
+    
+      });
+  
+      // if(feedData.length > 0){
+        
+      //   feedData.forEach(element => {
+      //     // console.log("element ===",element);
+      //     for (var j=0; j<CLIENTS.length; j++) {
+      //       CLIENTS[j].send(element);
+      //     }
+      //   });
+        
+      // }
+    
+    });
+  
+  
   function sendAll (message) {
       for (var i=0; i<CLIENTS.length; i++) {
           CLIENTS[i].send(message);
@@ -171,7 +238,7 @@ function runServer() {
         // setTimeout(() => {
         //   connect("ws://148.251.21.118:5570");
         // }, 1000);
-        connect("ws://148.251.21.118:5570",false);
+        // connect("ws://148.251.21.118:5570",false);
       };
   
       client.onerror = (error) => {
@@ -208,25 +275,13 @@ function runServer() {
 
   connect("ws://148.251.21.118:5570", false);
 
-  setInterval(() => {
 
-  var feedData = [];
-
-    sockApi.close();
-
-    runServer();
-      
-    }, 240000);
   
 }
-
-
 
 function runServerSetInterval() {
 
   var feedData = [];
-  
-  // var CLIENTS=[];
   
   
   function sendAll (message) {
