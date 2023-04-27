ebb08d770245ed38b4e8b65cf439e7df10ba2dbd
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-27 12:48:57 +0530

    change in index

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ const server = createServer(app);
 
 const wss = new WebSocket.Server({ server });
   
-  const wssMySock = new WebSocket('wss://hammerhead-app-hq3tv.ondigitalocean.app');
-  const sockApi = new WebSocket('ws://148.251.21.118:5570');
-  // const wssMySock = new WebSocket('wss://localhost:3000');
-    wssMySock.binaryType = "arraybuffer"; 
+socketLinks();
 
     var feedData = [];
   
@@ -88,6 +85,13 @@ server.listen(8080, function () {
 
 });
 
+function socketLinks() {
+  const wssMySock = new WebSocket('wss://hammerhead-app-hq3tv.ondigitalocean.app');
+  const sockApi = new WebSocket('ws://148.251.21.118:5570');
+  // const wssMySock = new WebSocket('wss://localhost:3000');
+    wssMySock.binaryType = "arraybuffer"; 
+}
+
 runServer();
 
 
@@ -97,7 +101,7 @@ setInterval(() => {
 
   sockApi.close();
 
-  runServer();
+  socketLinks();
   
   // runServerSetInterval();
     
@@ -108,66 +112,6 @@ function runServer() {
   var feedData = [];
   
   // var CLIENTS=[];
-  
-  wss.getUniqueID = function () {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    }
-    return s4() + s4() + '-' + s4();
-  };
-  
-  wss.on('connection', function connection(ws) {
-  
-      ws.id = wss.getUniqueID();
-  
-      CLIENTS.push(ws);
-  
-      ws.on('message', function message(messageData) {
-        let msg = JSON.parse(messageData);
-        console.log('received: %s', msg);
-        // console.log('received msg from: %s', ws);
-  
-        wss.clients.forEach(function each(client) {
-          if(client.id == ws.id) {
-              if(feedData.length > 0){
-              feedData.forEach(message => {
-                client.send(message);
-              });
-            }
-          }
-         });
-  
-        // if(messageData.pageData) {
-        // console.log('pageeee');
-  
-        // }
-  
-        // ws.close();
-        // ws.pong
-        // var msg = JSON.parse(messageData);
-        // sendAll(messageData);
-      //   wss.clients.forEach(function each(client) {
-        // wssMySock.send(messageData);
-      //    });
-        // if(messageData.pageData) {
-          // wssMySock.send(messageData);
-        // }
-        
-    
-      });
-  
-      // if(feedData.length > 0){
-        
-      //   feedData.forEach(element => {
-      //     // console.log("element ===",element);
-      //     for (var j=0; j<CLIENTS.length; j++) {
-      //       CLIENTS[j].send(element);
-      //     }
-      //   });
-        
-      // }
-    
-    });
   
   
   function sendAll (message) {
