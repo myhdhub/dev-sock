f2640f7d3d5a4d0f1ff64e55f8f33150045e793d
Mayur Dhamgaye <mayurd045@gmail.com>
2023-04-28 12:04:41 +0530

    change in index

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const wssMySock = new WebSocket('wss://hammerhead-app-hq3tv.ondigitalocean.app');
+//const wssMySock = new WebSocket('wss://hammerhead-app-hq3tv.ondigitalocean.app');
 // const wssMySock = new WebSocket('wss://localhost:3000');
-  wssMySock.binaryType = "arraybuffer"; 
+  //wssMySock.binaryType = "arraybuffer"; 
 
 var feedData = [];
 
@@ -46,36 +46,12 @@ wss.on('connection', function connection(ws) {
         }
        });
 
-      // if(messageData.pageData) {
-      // console.log('pageeee');
-
-      // }
-
-      // ws.close();
-      // ws.pong
-      // var msg = JSON.parse(messageData);
-      // sendAll(messageData);
-    //   wss.clients.forEach(function each(client) {
-      // wssMySock.send(messageData);
-    //    });
-      // if(messageData.pageData) {
-        // wssMySock.send(messageData);
-      // }
-      
-  
     });
-
-    // if(feedData.length > 0){
-     
-    //   feedData.forEach(element => {
-    //     for (var j=0; j<CLIENTS.length; j++) {
-    //       CLIENTS[j].send(element);
-    //     }
-    //   });
-       
-    // }
   
   });
+
+
+connect("ws://148.251.21.118:5570", false);
 
 
 function sendAll (message) {
@@ -84,32 +60,12 @@ function sendAll (message) {
     }
 }  
 
-
-
-// wss.on('connection', function (ws) {
-//   const id = setInterval(function () {
-//     ws.send(JSON.stringify(process.memoryUsage()), function () {
-//       //
-//       // Ignoring errors.
-//       //
-//     });
-//   }, 100);
-//   console.log('started client interval');
-
-//   ws.on('close', function () {
-//     console.log('stopping client interval');
-//     clearInterval(id);
-//   });
-// });
-
 server.listen(8080, function () {
   console.log('Listening on http://0.0.0.0:8080');
 });
 
 const connect = (endpoint,isReload) => {
   
-  
-
   try {
 
     let options = {
@@ -121,95 +77,57 @@ const connect = (endpoint,isReload) => {
     let client = new WebSocket(endpoint, options);
     client.binaryType = "arraybuffer";
 
-    
-
     client.onopen = () => {
       console.log("websocket open");
 
-      
         client.send(JSON.stringify({"requestType":1,"sessionKey":"","Data":{"Firm":"ALLOW","PrivateKey":"TT@90","ApiKey":"CRIC@20"}}));
-      //   client.send("42" + JSON.stringify(["getRooms", false]));
-
-      // wssMySock.onmessage = (event) => {
-      //   var enc = new TextDecoder("utf-8");
-                              
-      //   const decodeData = enc.decode(event.data); 
-      //   let recievedData = JSON.parse(decodeData);
-      //   if(recievedData.pageData) {
-      //     client.close();
-
-      //     client.send(JSON.stringify({"requestType":1,"sessionKey":"","Data":{"Firm":"ALLOW","PrivateKey":"TT@90","ApiKey":"CRIC@20"}}));
-      //   }
-      // };
 
     };
 
     client.onmessage = (event) => {
-      
 
-        // setInterval(() => {
-        //   feedData = [];
-        //   client.close();
-        // //   // console.log("length--", feedData.length);
-        // //   if(feedData.length > 1000) {
-        // //     let feedDataLength = feedData.length / 2;
-        // //     feedData.splice(0, feedDataLength);
-        // //   }
-        // // }, 900000);
-        // }, 900000);
-      if(event.data.responseType == 4) {
+      console.log("main--msg-",event.data);
+      
+      if(event.data.responseType == '4') {
         feedData.push(event.data);
       }
 
-        // if(feedData.length > 500) {
-        //   feedData.shift();
-        // }
-
         sendAll(event.data);  
-        // wss.send(event.data);
-        
-      // }, 1000);
-     
+            
     };
 
     client.onclose = (reason) => {
       console.log("websocket close:", reason.code);
-      // setTimeout(() => {
-      //   connect("ws://148.251.21.118:5570");
-      // }, 1000);
-      // connect("ws://148.251.21.118:5570",false);
+      
     };
 
     client.onerror = (error) => {
       console.log("websocket error:", error.message);
     };
 
-    wssMySock.onmessage = (event) => {
-      console.log("aa",event.data);
-      // var enc = new TextDecoder("utf-8");
+    // wssMySock.onmessage = (event) => {
+    //   console.log("aa",event.data);
+    //   // var enc = new TextDecoder("utf-8");
                             
-      // const decodeData = enc.decode(event.data); 
-      // let recievedData = JSON.parse(decodeData);
-      if(event.data.pageData) {
-        console.log("force close");
+    //   // const decodeData = enc.decode(event.data); 
+    //   // let recievedData = JSON.parse(decodeData);
+    //   if(event.data.pageData) {
+    //     console.log("force close");
         
-        client.close();
+    //     client.close();
 
-        console.log("uint8===",JSON.parse(decodeData));
-        // connect("ws://148.251.21.118:5570");
-        // client.close();
+    //     console.log("uint8===",JSON.parse(decodeData));
+    //     // connect("ws://148.251.21.118:5570");
+    //     // client.close();
 
-        // connect("ws://148.251.21.118:5570");
-        // location.href = '/'
+    //     // connect("ws://148.251.21.118:5570");
+    //     // location.href = '/'
     
-      }
-    // wss.send(event.data);
-    //   getMessage(event.data);
-    };
+    //   }
+    
+    // };
     
   } catch (error) {
     console.log("connect error:", error.message);
   }
 };
-
-connect("ws://148.251.21.118:5570", false);
